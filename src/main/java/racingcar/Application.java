e4ad36d08c565106a948e345bde5661f1030ba12
namonak <namonak@gmail.com>
2022-04-25 16:10:46 +0900

    feat: RacingCarGameModel 구현 및 generateWinner() 메서드에 대한 테스트 코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,57 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Application {
+    static class RacingCarGameModel {
+        private static final int MIN_NUM = 0;
+        private static final int MAX_NUM = 9;
+        private static final int PIVOT_VALUE = 3;
+
+        HashMap<String, Integer> racers = new HashMap<>();
+
+        String generateWinner(HashMap<String, Integer> racers) {
+            int maxValue = 0;
+            List<String> winners = new ArrayList<>();
+            for (Entry<String, Integer> entry : racers.entrySet()) {
+                if (entry.getValue() >= maxValue) {
+                    if (entry.getValue() != maxValue) {
+                        winners.clear();
+                    }
+                    maxValue = entry.getValue();
+                    winners.add(entry.getKey());
+                }
+            }
+            return String.join(", ", winners);
+        }
+
+        private void setCarUsers(String[] racerNames) {
+            for (String racer : racerNames) {
+                this.racers.put(racer, 0);
+            }
+        }
+
+        HashMap<String, Integer> run() {
+            for (Entry<String, Integer> entry : racers.entrySet()) {
+                if (pickNumberInRange(MIN_NUM, MAX_NUM) > PIVOT_VALUE) {
+                    racers.put(entry.getKey(), entry.getValue() + 1);
+                }
+            }
+            return racers;
+        }
+    }
+
+    static class RacingCarGameView {}
+
+    static class RacingCarGameController {}
+
+    static class Utils {}
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,9 +3,12 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -32,6 +35,62 @@ class ApplicationTest extends NsTest {
                 assertThat(output()).contains(ERROR_MESSAGE);
             }
         );
+    }
+
+    static class generateWinnerTestCase {
+        public String description;
+        public HashMap<String, Integer> car;
+        public String winner;
+    }
+
+    @Test
+    void 최종우승자_출력_테스트() {
+        generateWinnerTestCase[] tests = new generateWinnerTestCase[]{
+                new generateWinnerTestCase() {
+                    {
+                        description = "pobi, woni 우승";
+                        car = new HashMap<>();
+                        car.put("pobi", 2);
+                        car.put("woni", 2);
+                        winner = "pobi, woni";
+                    }
+                },
+                new generateWinnerTestCase() {
+                    {
+                        description = "pobi 우승";
+                        car = new HashMap<>();
+                        car.put("pobi", 2);
+                        car.put("woni", 1);
+                        winner = "pobi";
+                    }
+                },
+                new generateWinnerTestCase() {
+                    {
+                        description = "woni 우승";
+                        car = new HashMap<>();
+                        car.put("pobi", 1);
+                        car.put("woni", 2);
+                        winner = "woni";
+                    }
+                },
+                new generateWinnerTestCase() {
+                    {
+                        description = "pobi, woni 우승";
+                        car = new HashMap<>();
+                        car.put("pobi", 0);
+                        car.put("woni", 0);
+                        winner = "pobi, woni";
+                    }
+                },
+        };
+
+        Application.RacingCarGameModel racingCarGameModel = new Application.RacingCarGameModel();
+
+        for (generateWinnerTestCase test : tests) {
+            String winner = racingCarGameModel.generateWinner(test.car);
+
+            assertEquals(winner, test.winner, test.description);
+        }
     }
 
     @Override
