bee8d73f89b59808d0037b77388a28ce2026e8c6
namonak <namonak@gmail.com>
2022-10-09 12:27:00 +0900

    feat: add a function to compare position size

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -24,6 +24,10 @@ public class Position {
         this.position++;
     }
 
+    public boolean isBiggerPosition(Position position) {
+        return this.position >= position.position;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -2,6 +2,8 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,5 +14,14 @@ public class PositionTest {
         Position position = new Position();
         position.move();
         assertThat(position).isEqualTo(new Position(1));
+    }
+
+    @DisplayName("Position 크기 비교 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,false", "2,1,true", "1,1,true"}, delimiter = ',')
+    void compare(int position, int position2, boolean expected) {
+        Position pos = new Position(position);
+        Position pos2 = new Position(position2);
+        assertThat(pos.isBiggerPosition(pos2)).isEqualTo(expected);
     }
 }
