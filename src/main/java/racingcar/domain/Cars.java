47916700cc9a9fc8454eb2f008e03c3380024c22
namonak <namonak@gmail.com>
2022-10-09 14:55:03 +0900

    feat: add test code and production code for Cars
    The following have been implement and test:
     - add a class that manages a List<Car>
     - throw an IllegalArgumentException if only one car name is entered or duplicates are entered
     - add a function to find the winner among List<Car>

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,55 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        validateCars(cars);
+        this.cars = cars;
+    }
+
+    private static void validateCars(List<Car> cars) {
+        if (cars.size() <= 1) {
+            throw new IllegalArgumentException("자동차는 2대 이상 입력해주세요.");
+        }
+        if (cars.stream().distinct().count() != cars.size()) {
+            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+        }
+    }
+
+    public List<Car> getWinners() {
+        return makeWinnerList(getWinner());
+    }
+
+    private List<Car> makeWinnerList(Car winner) {
+        List<Car> winners = new ArrayList<>();
+        for (Car racer : cars) {
+            addWinner(winner, racer, winners);
+        }
+        return winners;
+    }
+
+    private static void addWinner(Car winner, Car car, List<Car> winners) {
+        if (car.compareWinner(winner)) {
+            winners.add(car);
+        }
+    }
+
+    private Car getWinner() {
+        Car winner = this.cars.get(0);
+        for (Car racer : cars) {
+            winner = findWinner(winner, racer);
+        }
+        return winner;
+    }
+
+    private static Car findWinner(Car winner, Car racer) {
+        if (racer.compareWinner(winner)) {
+            winner = racer;
+        }
+        return winner;
+    }
+}
