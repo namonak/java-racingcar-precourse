ac2a9c14a1b3efe659e22b398b351486e78b6e8c
namonak <namonak@gmail.com>
2022-10-10 01:14:01 +0900

    feat: add test code and production code for the race car
    The following have been implement and test:
     - throw an IllegalArgumentException if the name input value does not include a comma(',')
     - throw an IllegalArgumentException if the number of attempts input is less than or equal to 0
     - implement the function to create a Car object after separating the input string with commas
     - implement a function that starts the racing game after validating the input

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,40 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+    public static final int MIN_TRY_COUNT = 1;
+    private final Cars cars;
+
+    public RacingGame(String input, int tryCount) {
+        validateInput(input, tryCount);
+        this.cars = new Cars(makeCarList(separateInput(input)));
+        this.cars.startRacing(tryCount);
+    }
+
+    private void validateInput(String input, int tryCount) {
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException("자동차 이름은 쉼표(,)로 구분해 주세요.");
+        }
+        if (tryCount < MIN_TRY_COUNT) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+    }
+
+    private List<String> separateInput(String input) {
+        List<String> carNames = new ArrayList<>();
+        for (String carName : input.split(",")) {
+            carNames.add(carName.trim());
+        }
+        return carNames;
+    }
+
+    private static ArrayList<Car> makeCarList(List<String> cars) {
+        ArrayList<Car> carList = new ArrayList<>();
+        for (String car : cars) {
+            carList.add(new Car(car));
+        }
+        return carList;
+    }
+}
