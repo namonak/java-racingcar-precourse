7671f6ddf933f69b52661fd8749d95266139f2dc
namonak <namonak@gmail.com>
2022-10-10 00:40:29 +0900

    feat: add a function that generates a random number
    The following have been implement and test:
     - add a function to generate a random number in the range 0-9

--- src/main/java/racingcar/util/Utils.java
+++ src/main/java/racingcar/util/Utils.java
@@ -1,7 +1,16 @@
 package racingcar.util;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Utils {
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 9;
+
     public static boolean isAlphabet(String str) {
         return str.matches("^[a-zA-Z]*$");
+    }
+
+    public static int generateRandomNumber() {
+        return pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
--- src/test/java/racingcar/util/UtilsTest.java
+++ src/test/java/racingcar/util/UtilsTest.java
@@ -1,6 +1,7 @@
 package racingcar.util;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,7 +11,13 @@ public class UtilsTest {
     @DisplayName("알파벳 입력 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"pobi", "crong", "honux"})
-    void isAlphabet(String given) {
+    void isAlphabetTest(String given) {
         assertThat(Utils.isAlphabet(given)).isTrue();
+    }
+
+    @DisplayName("0~9 범위의 임의의 숫자 생성 테스트")
+    @Test
+    void generateRandomNumberTest() {
+            assertThat(Utils.generateRandomNumber()).isBetween(0, 9);
     }
 }
