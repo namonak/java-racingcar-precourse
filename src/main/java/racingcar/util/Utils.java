7756cca53432c247e322c6fd410636c0c2ce26f5
namonak <namonak@gmail.com>
2022-10-10 13:43:13 +0900

    feat: add a function to verify the input tryCount to the Utils class
    The following have been implement and test:
     - if the input value is not a number, throw a NumberFormatException
     - if the input number is less than 1, throw an IllegalArgumentException

--- src/main/java/racingcar/util/Utils.java
+++ src/main/java/racingcar/util/Utils.java
@@ -5,6 +5,7 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class Utils {
     public static final int MIN_NUMBER = 0;
     public static final int MAX_NUMBER = 9;
+    public static final int MIN_TRY_COUNT = 1;
 
     public static boolean isAlphabet(String str) {
         return str.matches("^[a-zA-Z]*$");
@@ -12,5 +13,25 @@ public class Utils {
 
     public static int generateRandomNumber() {
         return pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+
+    public static int validateTryCount(String input) {
+        if (!isNumber(input)) {
+            throw new NumberFormatException("숫자만 입력 가능합니다.");
+        }
+        int tryCount = Integer.parseInt(input);
+        if (tryCount < MIN_TRY_COUNT) {
+            throw new IllegalArgumentException("1 이상의 숫자만 입력 가능합니다.");
+        }
+        return tryCount;
+    }
+
+    public static boolean isNumber(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- src/test/java/racingcar/util/UtilsTest.java
+++ src/test/java/racingcar/util/UtilsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UtilsTest {
     @DisplayName("알파벳 입력 테스트")
@@ -19,5 +20,17 @@ public class UtilsTest {
     @Test
     void generateRandomNumberTest() {
             assertThat(Utils.generateRandomNumber()).isBetween(0, 9);
+    }
+
+    @DisplayName("tryCount 검증 테스트")
+    @Test
+    void validateTryCountTest() {
+        assertThatThrownBy(() -> Utils.validateTryCount("0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 이상의 숫자만 입력 가능합니다.");
+
+        assertThatThrownBy(() -> Utils.validateTryCount("pobi"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자만 입력 가능합니다.");
     }
 }
